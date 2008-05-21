92800b4c0734e2d61366d3f27a07f44691ddd691
webchemist <webchemist@e19c5816-0549-0410-8ac3-0dfbdefe5972>
2008-05-21 19:43:19 +0000

    Изменено меню клиента - введено указание пунктов только для TrustedUsers

--- client/templatetags/menu.py
+++ client/templatetags/menu.py
@@ -9,11 +9,20 @@ menu_items = [
     {'action':'import_order','name':"Загрузить XLS",'access':'user'},
 ]
 
+trusted_actions = ('cp',)
+
 @register.inclusion_tag('client/tags/menu.html')
-def client_menu(current_action):
-    for data in menu_items:
+def client_menu(current_action, user):
+    menu = []
+    for i in menu_items :
+        if i['action'] in trusted_actions :
+            if user.is_trusted() :
+                menu.append(i) 
+        else:
+            menu.append(i)
+    for data in menu :
         if data['action'] == current_action :
             data['class'] = 'current'
         else :
             data['class'] = 'usual'               
-    return {'items': menu_items}
+    return {'items': menu}
