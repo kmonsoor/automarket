54d74e2277f72f3517af3fc1ddcb090d7e7f99c7
ermalinsky@gmail.com <ermalinsky@gmail.com@e19c5816-0549-0410-8ac3-0dfbdefe5972>
2010-11-28 19:49:04 +0000

    кастомизация User в админке

--- data/admin.py
+++ data/admin.py
@@ -2,8 +2,11 @@
 # $Id$
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User as DjangoUser
 from django.utils.translation import ugettext_lazy as _
 from data.models import Brand, OrderedItem, Supplier
+admin.site.unregister(DjangoUser)
 
 class SupplierAdmin(admin.ModelAdmin):
     list_display = ('title', 'direction', 'po', 'description',)
@@ -16,7 +19,11 @@ class OrderedItemOptions(admin.ModelAdmin):
     list_display =('ponumber', 'created', 'manager', 'client','part_number','part_number_superseded','quantity')
     list_filter = ('created',)
     search_fieldsets = ('part_number',)
+    
+class MyUserAdmin(UserAdmin):
+    list_display = ('id', 'username', 'email','first_name', 'last_name', 'is_staff', 'last_login',)
 
 admin.site.register(Supplier, SupplierAdmin)
 admin.site.register(Brand, BrandOptions)
 admin.site.register(OrderedItem, OrderedItemOptions)
+admin.site.register(DjangoUser, MyUserAdmin)
