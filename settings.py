257e510d90215f5277ea662707bf2937aa15017d
ermalinsky@gmail.com <ermalinsky@gmail.com@e19c5816-0549-0410-8ac3-0dfbdefe5972>
2011-03-23 21:11:36 +0000

    тип доставки по умолчанию - 1 (Avia)

--- settings.py
+++ settings.py
@@ -98,7 +98,7 @@ SOAP_LOGIN = 'SP'
 SOAP_PASSWORD = 'newmotors'
 WSDL_URL = 'http://automototrade.com/wsdl/server.php?wsdl'
 
-DELIVERY_TYPE = 3
+DELIVERY_TYPE = 1 # Avia
 
 # SMTP
 EMAIL_HOST = 'localhost'
